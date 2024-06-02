6f83697ffd4858b16653677d990f2000c3bd0244
Soumen3 <soumensamanta112233@gmail.com>
2024-06-03 01:30:03 +0530

    main banner created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Banner from './components/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className=' bg-slate-200'>Portfolilo</h1>
+     <NavBar />
+     <Banner />
     </>
   )
 }
