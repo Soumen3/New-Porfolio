55c6ea5adbd239ba28380e055fddcbe10855dcd7
Soumen3 <soumensamanta112233@gmail.com>
2024-06-04 12:17:16 +0530

    project and contact

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import NavBar from './components/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Banner'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +16,9 @@ function App() {
     <>
      <NavBar />
      <Banner />
-     <Skills/>
+     <Skills />
+     <Projects />
+     <Contact />
     </>
   )
 }
